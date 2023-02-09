a0de34fc57fa41be8fc1dc94b408ada15ff6f2b0
Deepanshu003 <91359646+Deepanshu003@users.noreply.github.com>
2023-02-08 20:43:54 -0800

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAv_gCcgpE_XkjRkHUbRSCHnKklMiEoBtE",
+    authDomain: "linkedin-clone-456e7.firebaseapp.com",
+    databaseURL: "https://linkedin-clone-456e7-default-rtdb.firebaseio.com",
+    projectId: "linkedin-clone-456e7",
+    storageBucket: "linkedin-clone-456e7.appspot.com",
+    messagingSenderId: "309808097732",
+    appId: "1:309808097732:web:c008873918fee0a01d5c91",
+    measurementId: "G-WECP8Z617B"
+  };
+  // const firebaseApp = firebase.initializeApp(firebaseConfig);
+  // const db = firebaseApp.firestore();
+  // const auth = firebase.auth();
+  // const provider = new firebase.auth.GoogleAuthProvider();
+  // const storage = firebase.storage();
+  // export{auth,provider,storage,db};
+  export default firebaseConfig;
